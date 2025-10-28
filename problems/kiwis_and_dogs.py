bbb9b9e850a924b8bdded99c6fe779e2bf0350b9
Paul <lazaro.roca.pol@gmail.com>
2025-10-28 01:59:22 +0100

    First kiwi_and_dogs version (not working)

--- problems/kiwis_and_dogs.py
+++ problems/kiwis_and_dogs.py
@@ -52,9 +52,65 @@ class KiwisAndDogsProblem(Problem):
         return [State(kiwis=("D", "F"), dogs=("C",))]
 
     def is_goal_state(self, state):
-        raise NotImplementedError("Implement me!")
+        return state == State(
+        kiwis=[("A",) for _ in range(self.num_kiwis)],
+        dogs=[("E",) for _ in range(self.num_dogs)]
+        ) # TODO: Ask Edu if this is valid
 
     def is_valid_state(self, _):
-        raise NotImplementedError("Implement me!")
+        True # Asegurem que sigui valid des de les accions
 
-    # Actions go here...
+
+    # TODO: Trobar manera de referenciar el cost de graph, he posat A i B com a exemple del que vull fer
+    # Movem tots els dogs disponibles, del primer punt possible al segon punt possible, cost definit per graph dinamicament
+    @action(DDRange(0, 'num_dogs'), Categorical(["A", "B", "C", "D", "E", "F", "G"]), cost=1)
+    def moveDog(self, state, dog_id, dog_to):
+        
+        dog_from = state.dogs[dog_id]
+        
+        # Asegurem de que existeix la aresta
+        try:
+            if self.graph[dog_from, dog_to] is not None:
+                cond = self.graph[dog_from, dog_to][1] # somebody(C)
+                
+                #Comprovem que hi ha algú al node que hi ha dins del sombody(X)
+                if "somebody" in cond and (cond[-2:-1] in state.kiwis or cond[-2:-1] in state.dogs):
+                    # Recordem que no podem modificar una tupla
+                    new_dogs = list(state.dogs)
+                    new_dogs[dog_id] = dog_to
+                    return State(kiwis=state.kiwis, dogs=tuple(new_dogs))
+                
+                if "nobody" in cond and (cond[-2:-1] not in state.kiwis or cond[-2:-1] not in state.dogs):
+                    new_dogs = list(state.dogs)
+                    new_dogs[dog_id] = dog_to
+                    return State(kiwis=state.kiwis, dogs=tuple(new_dogs))
+                
+        except KeyError:
+            return None
+
+    # Movem tots els kiwis disponibles, del primer punt possible al segon punt possible, cost definit per graph dinamicament
+    @action(DDRange(0, 'num_kiwis'), Categorical(["A", "B", "C", "D", "E", "F", "G"]), cost=1)
+    def moveKiwi(self, state, kiwi_id, kiwi_to):
+        
+        kiwi_from = state.kiwis[kiwi_id]
+        
+        try:
+            # Asegurem de que existeix la aresta
+            if self.graph[kiwi_from, kiwi_to] is not None:
+                cond = self.graph[(kiwi_from, kiwi_to)][1] # somebody(C)
+                
+                #Comprovem que hi ha algú al node que hi ha dins del sombody(X)
+                if "somebody" in cond and (cond[-2:-1] in state.kiwis or cond[-2:-1] in state.dogs):
+                    new_kiwis = list(state.kiwis)
+                    new_kiwis[kiwi_id] = kiwi_to
+                    return State(kiwis=tuple(new_kiwis), dogs=state.dogs)
+                
+                if "nobody" in cond and (cond[-2:-1] not in state.kiwis or cond[-2:-1] not in state.dogs):
+                    new_kiwis = list(state.kiwis)
+                    new_kiwis[kiwi_id] = kiwi_to
+                    return State(kiwis=tuple(new_kiwis), dogs=state.dogs)
+
+                return None
+            
+        except KeyError:
+            return None
---
+++ test.py
@@ -0,0 +1,4 @@
+const = "hola(X)"
+print(const[-2:-1])
+print("somebody" in "somebody(X)")
+print("X" in tuple(["X"]))
